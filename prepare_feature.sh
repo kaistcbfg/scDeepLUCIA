eb214e2c55396ae4fe780b01b1b82aba3dafc504
yang-kaist <45616714+yang-kaist@users.noreply.github.com>
2025-03-15 13:40:43 +0900

    Create prepare_feature.sh

---
+++ prepare_feature.sh
@@ -0,0 +1,4 @@
+ mkdir -p feature/positive_loops/mm10/B001/   ; wget http://junglab.kaist.ac.kr/Dataset/scDeepLUCIA/positive_loop.chr10.txt   -O feature/positive_loops/mm10/B001/positive_loop.chr10.txt
+ mkdir -p feature/sliced_con_array/mm10/B001/ ; wget http://junglab.kaist.ac.kr/Dataset/scDeepLUCIA/contact_030M.chr10.npy    -O feature/sliced_con_array/mm10/B001/contact_030M.chr10.npy
+ mkdir -p feature/sliced_epi_array/mm10/B001/ ; wget http://junglab.kaist.ac.kr/Dataset/scDeepLUCIA/r2_030M.chr10.npy         -O feature/sliced_epi_array/mm10/B001/r2_030M.chr10.npy
+ mkdir -p feature/sliced_seq_array/mm10/isHC/ ; wget http://junglab.kaist.ac.kr/Dataset/scDeepLUCIA/isHC.seq_onehot.chr10.npy -O feature/sliced_seq_array/mm10/isHC/isHC.seq_onehot.chr10.npy
